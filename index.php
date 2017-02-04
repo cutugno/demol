09a947650fcb696690573dc65def1f4b7717340e
Ale@ubuasus <sberz666@gmail.com>
2017-02-04 10:46:30 +0100

    testing remote git

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <title>L'Autodemolizione di Roma - L'unica con la "L"</title>
+        <title>L'Autodemolizione di Roma - L'unico con la "L"</title>
         <!-- Bootstrap -->
         <link href="<?php echo BASE_URL ?>/bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <link href="<?php echo BASE_URL ?>/css/animate.css" rel="stylesheet">
